61105e199548d9b485f70a591ce04929a78b8ef2
Rohan <rohan26ir@gmail.com>
2025-02-02 00:09:55 +0600

    add image with ring

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ import { PiMountainsFill } from 'react-icons/pi';
 import { Link, NavLink } from 'react-router-dom';
 
 import ring from '../../assets/ring.webp';
+import avater from '../../assets/avater.webp';
 
 const Navbar = () => {
-
   const navbar = (
     <>
       <li><NavLink to={'/'}>Home</NavLink></li>
@@ -17,7 +17,7 @@ const Navbar = () => {
           <ul className="p-2 w-[160%] -left-[30%]">
             <li><NavLink to={'/city/lymhurst'}><MdForest />Lymhurst</NavLink></li>
             <li><NavLink to={'/city/bridgewatch'}><GiDesert />Bridgewatch</NavLink></li>
-            <li><NavLink to={'/city/fortSterling'}><PiMountainsFill />Fort Sterling </NavLink></li>
+            <li><NavLink to={'/city/fortSterling'}><PiMountainsFill />Fort Sterling</NavLink></li>
             <li><NavLink to={'/city/martlock'}><MdOutlineLandslide />Martlock</NavLink></li>
             <li><NavLink to={'/city/thetford'}><GiSwamp />Thetford</NavLink></li>
           </ul>
@@ -37,7 +37,7 @@ const Navbar = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
               </svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow">
               {navbar}
             </ul>
           </div>
@@ -49,22 +49,20 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <div className="dropdown dropdown-end">
+          <div className="dropdown dropdown-end mr-3">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar relative">
-              {/* Ring Image as Border */}
-              <div className="absolute inset-0 flex items-center justify-center">
-                <img src={ring} alt="ring border" className="w-14 h-14 object-contain" />
-              </div>
-              {/* Avatar Image */}
-              <div className="w-10 h-10 rounded-full overflow-hidden relative z-10">
+              <div className="w-20 h-20 rounded-full relative z-10 overflow-hidden">
                 <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                  className="w-full h-full"
+                  alt="User Avatar"
+                  src={avater}
+                  className="w-full h-full object-cover"
                 />
               </div>
+              <div className="w-20 h-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0">
+                <img src={ring} alt="Ring Overlay" className="w-full h-full object-contain" />
+              </div>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow">
               <li>
                 <a className="justify-between">
                   Profile
