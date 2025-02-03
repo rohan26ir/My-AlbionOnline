c7983414f2c45fa644e5426ce8a2e10b8f8471a2
Rohan <rohan26ir@gmail.com>
2025-02-04 00:37:10 +0600

    add provider with basic

---
+++ src/Provider/Provider.jsx
@@ -0,0 +1,37 @@
+import { createUserWithEmailAndPassword, GoogleAuthProvider } from '@firebase/auth';
+import React, { useState } from 'react';
+import { createContext } from 'react';
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+
+
+
+const Provider = ({ children }) => {
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email , password)
+  }
+  
+  
+  const authInfo = {
+  
+  }
+
+
+  return (
+    <AuthContext.Provider  value={"authInfo"}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default Provider;
